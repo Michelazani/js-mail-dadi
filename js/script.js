f1a8ada767e6f80de1d84faf766a25244ced5773
Michelazani <michi.zani220@gmail.com>
2023-11-15 16:00:41 +0100

    numero casuale

---
+++ js/script.js
@@ -0,0 +1,20 @@
+// Gioco dei dadi
+// Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
+// Stabilire il vincitore, in base a chi fa il punteggio più alto.
+
+const randomNumberComputer = Math.floor ( Math.random () * 6) + 1; 
+    console.log(randomNumberComputer);
+    
+
+
+
+
+
+
+
+//? Mail
+// ?  Chiedi all’utente la sua email, controlla che sia nella lista di chi può accedere (createvi una piccola lista di email),
+// ?  stampa un messaggio appropriato sull’esito del controllo.
+// ?  Prima di partire a scrivere codice poniamoci qualche domanda:
+// ?  Che ci sia un array da qualche parte?
+// ?  Se dobbiamo confrontare qualcosa che "cosa" ci serve?
