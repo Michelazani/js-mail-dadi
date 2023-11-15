541412fdb6687cd395d90f2ed28fa83abc7e39f4
Michelazani <michi.zani220@gmail.com>
2023-11-15 17:13:36 +0100

    creato arrey

--- js/script.js
+++ js/script.js
@@ -12,7 +12,7 @@ buttonElement.addEventListener ('click' , function (){
     const randomNumberComputer = Math.floor ( Math.random () * 6) + 1; 
         console.log(randomNumberComputer)
     const randomNumberUser = Math.floor ( Math.random () * 6) + 1;
-        console.log(randomNumberUser)
+        console.log(randomNumberUser + 'your random number')
 
     if(randomNumberComputer > randomNumberUser){
         alert('Computer won')
@@ -28,13 +28,35 @@ buttonElement.addEventListener ('click' , function (){
 
 
 
+//! Mail
+// !  Chiedi all’utente la sua email, controlla che sia nella lista di chi può accedere (createvi una piccola lista di email),
+// !  stampa un messaggio appropriato sull’esito del controllo.
+// !  Prima di partire a scrivere codice poniamoci qualche domanda:
+// !  Che ci sia un array da qualche parte?
+// !  Se dobbiamo confrontare qualcosa che "cosa" ci serve?
+
+const sendElement = document.getElementById('sendButton');
+const mailTyped = document.getElementById ('mailUser');
 
 
+const controlMailUser = [
+    'ciao@gmail.com',
+    'ginetto@gmail.com',
+    'gina@gmail.com',
+    'giorgione@gmail.com'
+];
 
+sendElement.addEventListener ('click' , function (){
 
-//? Mail
-// ?  Chiedi all’utente la sua email, controlla che sia nella lista di chi può accedere (createvi una piccola lista di email),
-// ?  stampa un messaggio appropriato sull’esito del controllo.
-// ?  Prima di partire a scrivere codice poniamoci qualche domanda:
-// ?  Che ci sia un array da qualche parte?
-// ?  Se dobbiamo confrontare qualcosa che "cosa" ci serve?
+    for (let i = 1; i <= controlMailUser.length ; i++){
+        if(controlMailUser === mailTyped ){
+            controlMailUser.push (i)
+            alert('correct email')
+        }
+        else if( mailTyped != controlMailUser){
+            controlMailUser.push (i)
+            alert('try again')
+            }
+   }
+} ) 
+
